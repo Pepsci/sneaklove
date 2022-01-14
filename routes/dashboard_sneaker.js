6adc4da584b85e76c0723d987c8d9ab16d1825ae
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-01-14 13:05:46 +0100

    done edit delete

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -14,26 +14,31 @@ router.get("/prod-add", async (req, res) => {
     }
 });
 
-router.get("/prod-manage", (req, res) => {
-  res.render("products_manage.hbs");
+router.get("/prod-manage", async (req, res, next) => {
+try {
+  res.render("products_manage.hbs", { sneakers : await sneakerModel.find().populate('tag')});
+}
+catch(e){
+  next(e)
+}
 });
 
 router.post("/tag", async (req, res) => {
     const newTag = {...req.body};
  try{
      await tagModel.create(newTag);
-    res.redirect("prod-add")
+    res.redirect("/prod-add")
  }
   catch(e) {
       next(e)
   }  
 })
 
-router.post("/", uploader.single("picture"), async (req, res, next) => {
+router.post("/prod-add", uploader.single("image"), async (req, res, next) => {
   const newSneaker = { ...req.body };
 
-  if (!req.file) newSneaker.picture = undefined;
-  else newSneaker.picture = req.file.path;
+  if (!req.file) newSneaker.image = undefined;
+  else newSneaker.image = req.file.path;
   try {
     await sneakerModel.create(newSneaker);
     res.redirect("/prod-manage");
@@ -43,6 +48,37 @@ router.post("/", uploader.single("picture"), async (req, res, next) => {
   }
 });
 
-router.post
+router.get("/product-edit/:id", async (req, res, next) => {
+  try {
+    const tags = await tagModel.find()
+    const sneaker = await sneakerModel.findById(req.params.id).populate('tag')
+    res.render("product_edit", { sneaker, tags})
+  }
+  catch(e){
+    next(e)
+  }
+});
+
+router.post("/product-edit/:id", uploader.single("image"), async (req, res, next) => {
+  try { 
+    const updatedSneaker = {...req.body};
+    if (req.file) updatedSneaker.image = req.file.path
+    await sneakerModel.findByIdAndUpdate(req.params.id, updatedSneaker);
+    console.log(updatedSneaker)
+    res.redirect("/prod-manage")
+  }
+  catch(e){
+    next(e)
+  }
+})
+
+router.get("/product-delete/:id", async (req, res, next) => {
+  try {
+    await sneakerModel.findByIdAndRemove(req.params.id);
+    res.redirect("/prod-manage");
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
